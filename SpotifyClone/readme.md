41b7c107b9cc17ccf184dc4be7d7a531deca3afb
Raunak Dey <raunakdey41@gmail.com>
2025-06-26 12:18:54 +0530

    Change logo, update README and integrate Jamendo API

--- SpotifyClone/readme.md
+++ SpotifyClone/readme.md
@@ -1,10 +1,27 @@
-# Music Streaming platform inspired by Spotify
+# 🎵 Dummy Music Streaming Platform
 
-Tech stack used:
-    HTML
-    CSS
-    Javascript
+A front-end project loosely inspired by Spotify, built purely with HTML, CSS, and JavaScript.
 
-Songs and Images used are from Pixabay royalty free images and songs.
+This is a conceptual clone with a custom UI and layout, created for learning and experimentation.
 
-Icons used are from svgrepo
+# 🛠️ Tech Stack
+
+HTML5 for structure
+
+CSS3 for styling and layout
+
+Vanilla JavaScript for interactivity and functionality
+
+# 📁 Assets & Credits
+
+Songs & Images: Any kind of media used is from https://developer.jamendo.com/v3.0.
+
+Icons: SVG icons are used from https://www.svgrepo.com/.
+
+# 🎨 Creative Liberty
+
+This project is a creative interpretation and not an exact replica of Spotify.
+
+UI/UX decisions, layout styling, and features were made with flexibility and originality in mind.
+
+# This project is non-commercial and uses the Jamendo API under its free tier. All music is royalty-free and intended for educational purposes only.
--- SpotifyClone/script.js
+++ SpotifyClone/script.js
@@ -1,11 +1,31 @@
+// Private id
+const id = "da69b01d";
+const url = `https://api.jamendo.com/v3.0/tracks/?client_id=${id}&format=json&limit=42&search=chill`
+
 let currentState; //To access the current state of any attribute
+let displayBar = document.getElementById("display"); //The dash sign for showing/hiding the bar
+let bar = document.getElementById("music-control");//The footer section
+let playPause = document.getElementById("play"); // Play button on the media control bar
+let songNames = document.querySelectorAll(".song-tiles");
+console.log(songNames); // Store arrays of the song tiles
+let songContainer = document.querySelectorAll(".song-container"); // Store the entire box of songs
+let songImages = document.querySelectorAll(".song-tile-images"); // Store the images of the songs
+
+document.addEventListener("DOMContentLoaded", function(){
+    fetchSongs();
+})
+
+// Toggling between play and pause for songs
+playPause.addEventListener("click", function(){
+    currentState = playPause.getAttribute("src");
+    if(currentState.includes("icons/play.svg"))
+        playPause.setAttribute("src", "icons/pause.svg");
+    else
+        playPause.setAttribute("src", "icons/play.svg");
+    currentState = "";
+});
 
 // Toggling between show and hide for the display bar
-
-let displayBar = document.getElementById("display"); //The dash sign for showing/hiding the bar
-
-let bar = document.getElementById("music-control");//The footer section
-
 displayBar.addEventListener("click", function(){
     currentState = displayBar.getAttribute("title"); //Current condition of the display bar
 
@@ -16,40 +36,35 @@ displayBar.addEventListener("click", function(){
         displayBar.setAttribute("title", "Show Bar");
         for(let i = 0; i < playBarIcons.length; i++){
             playBarIcons[i].style.cssText = 
-            `width: 0;
-             height: 0; 
-             display: none;`
+            `
+            width: 0;
+            height: 0; 
+            display: none;
+            `
         }
+        bar.style.transitionDuration = "500ms";
     }
     else{
         bar.style.height = "11vh";
         displayBar.setAttribute("title", "Hide Bar");
         for(let i = 0; i < playBarIcons.length; i++){
             playBarIcons[i].style.cssText = 
-            `width: 2.5em;
-             height: 2.5em;`
+            `
+            width: 2.5em;
+            height: 2.5em;
+            `
         }
+        bar.style.transitionDuration = "500ms";
     }
 });
 
+for(let obj in songImages){
+    console.log(songImages[obj]);
+}
 
-// Toggling between play and pause for songs
-
-let playPause = document.getElementById("play");
-
-playPause.addEventListener("click", function(){
-    currentState = playPause.getAttribute("src");
-    if(currentState.includes("icons/play.svg"))
-        playPause.setAttribute("src", "icons/pause.svg");
-    else
-        playPause.setAttribute("src", "icons/play.svg");
-    currentState = "";
-});
-
-// Playing on the songs
-
-let songNames = document.querySelectorAll(".song-tiles");
-console.log(songNames);
-
-let songContainer = document.querySelectorAll(".song-container");
-
+// Fetch songs from jamendo
+async function fetchSongs() {
+    let songs = await fetch(url);
+    songs = await songs.json();
+    console.log(songs);
+}
